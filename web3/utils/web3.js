885b54f6f50e13c0b0c088b2af8915ae1e190a34
rithvik <rithvikbng@gmail.com>
2025-03-02 20:47:58 +0530

    updates in address

--- web3/utils/web3.js
+++ web3/utils/web3.js
@@ -4,7 +4,7 @@ import DAOABI from "../artifacts/contracts/DAO.sol/DAO.json";
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
-const jobPortal = new ethers.Contract("YOUR_DEPLOYED_JOB_PORTAL_ADDRESS", JobPortalABI.abi, signer);
-const dao = new ethers.Contract("YOUR_DEPLOYED_DAO_ADDRESS", DAOABI.abi, signer);
+const jobPortal = new ethers.Contract("Y0x1234567890abcdef1234567890abcdef12345678", JobPortalABI.abi, signer);// Replace with your deployed contract address
+const dao = new ethers.Contract("0xabcdef1234567890abcdef1234567890abcdef12", DAOABI.abi, signer);// Replace with your deployed contract address
 
 export { jobPortal, dao };

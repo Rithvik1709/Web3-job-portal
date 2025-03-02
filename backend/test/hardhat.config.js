1ade7f64c556e8603d96df46ed65bba8f5f6f849
rithvik <rithvikbng@gmail.com>
2025-03-02 19:55:50 +0530

    hardhat config files added

---
+++ backend/test/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
